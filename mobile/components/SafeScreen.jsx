acada75918d19fda39884244717382283e8ff872
Pomelo418 <eunice.yan01@hotmail.com>
2025-12-16 21:31:57 -0500

    finished favorites screen

--- mobile/components/SafeScreen.jsx
+++ mobile/components/SafeScreen.jsx
@@ -2,7 +2,15 @@ import { View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { COLORS } from "../constants/colors";
 
-const SafeScreen = ({ children }) => {
+const SafeScreen = ({ children }) => { 
+
+  /*
+  This code defines a reusable component in React Native
+  called SafeScreen that helps manage the safe display
+  of content on devices with screen "notches," status 
+  bars, or bottom navigation areas (like the iPhone X's 
+  home indicator area). 
+  */
   const insets = useSafeAreaInsets();
 
   return (
